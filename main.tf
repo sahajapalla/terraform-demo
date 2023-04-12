71522435083ca7b5fd34247661673992374607fc
sahaja palla <sahaja.palla15@gmail.com>
2023-04-12 06:00:36 +0000

    edited main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "ec2_public" {
     ami = var.ami_id
     instance_type = var.instance_type
     key_name = var.key_name
-    security_groups = ["${aws_security_group.nishant-ssh-security-group.id}"]
+    security_groups = ["${aws_security_group.sahaja-ssh-security-group.id}"]
     subnet_id = aws_subnet.public-subnet-1.id
     associate_public_ip_address = true
     lifecycle {
